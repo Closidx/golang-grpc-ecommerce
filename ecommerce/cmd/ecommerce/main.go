ef3fa8fc12d25d5bd19913c0b073b9b72bdba02c
Closidx <Coffeekostarev@gmail.com>
2024-06-21 23:18:57 +0300

    add graceful shutdown

--- ecommerce/cmd/ecommerce/main.go
+++ ecommerce/cmd/ecommerce/main.go
@@ -5,6 +5,8 @@ import (
 	"github/closidx/golang-grpc-ecommerce/ecommerce/internal/config"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 const (
@@ -22,7 +24,18 @@ func main() {
 
 	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath)
 
-	application.GRPCSrv.MustRun()
+	go application.GRPCSrv.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <- stop
+
+	log.Info("stopping application", slog.String("signal", sign.String()))
+
+	application.GRPCSrv.Stop()
+
+	log.Info("application stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
